bd12978e6a3949a04c04613dfbc1911e90091810
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-11 23:38:57 -0300

    Added Recipe and Resource model

---
+++ app/src/main/java/com/wilderpereira/reciclo/Models/Recipe.java
@@ -0,0 +1,45 @@
+package com.wilderpereira.reciclo.Models;
+
+import java.util.List;
+
+/**
+ * Created by Wilder on 11/07/16.
+ */
+public class Recipe {
+    String name;
+    String preparation;
+    int recycleCount;
+    List<Resource> resouces;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+
+    public int getRecycleCount() {
+        return recycleCount;
+    }
+
+    public void setRecycleCount(int recycleCount) {
+        this.recycleCount = recycleCount;
+    }
+
+    public List<Resource> getResouces() {
+        return resouces;
+    }
+
+    public void setResouces(List<Resource> resouces) {
+        this.resouces = resouces;
+    }
+}
---
+++ app/src/main/java/com/wilderpereira/reciclo/Models/Resource.java
@@ -0,0 +1,34 @@
+package com.wilderpereira.reciclo.Models;
+
+/**
+ * Created by Wilder on 11/07/16.
+ */
+public class Resource {
+    int amount;
+    String name;
+    String type;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
