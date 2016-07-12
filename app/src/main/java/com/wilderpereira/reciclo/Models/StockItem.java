d68249be7502ec3963342ccdb84ca26b78df2ff8
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-11 23:23:39 -0300

    Add StockItem model

---
+++ app/src/main/java/com/wilderpereira/reciclo/Models/StockItem.java
@@ -0,0 +1,26 @@
+package com.wilderpereira.reciclo.Models;
+
+/**
+ * Created by Wilder on 11/07/16.
+ */
+public class StockItem {
+
+    String name;
+    int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
