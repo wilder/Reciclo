5881a78e0fd4987cc7da6fe1e56b2014f4cf30fb
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-11 23:46:53 -0300

    Added Favorites and Stock models

---
+++ app/src/main/java/com/wilderpereira/reciclo/Models/Favorites.java
@@ -0,0 +1,18 @@
+package com.wilderpereira.reciclo.Models;
+
+import java.util.List;
+
+/**
+ * Created by Wilder on 11/07/16.
+ */
+public class Favorites {
+    List<Recipe> favoriteRecipes;
+
+    public List<Recipe> getFavoriteRecipes() {
+        return favoriteRecipes;
+    }
+
+    public void setFavoriteRecipes(List<Recipe> favoriteRecipes) {
+        this.favoriteRecipes = favoriteRecipes;
+    }
+}
---
+++ app/src/main/java/com/wilderpereira/reciclo/Models/Stock.java
@@ -0,0 +1,19 @@
+package com.wilderpereira.reciclo.Models;
+
+import java.util.List;
+
+/**
+ * Created by Wilder on 11/07/16.
+ */
+public class Stock {
+
+    List<StockItem> stockItems;
+
+    public List<StockItem> getStockItems() {
+        return stockItems;
+    }
+
+    public void setStockItems(List<StockItem> stockItems) {
+        this.stockItems = stockItems;
+    }
+}
