34120217fdf061f6f8e4acbc2c1b2c3eeb79adeb
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-19 22:29:54 -0300

    Setup navigation drawer

--- app/src/main/java/com/wilderpereira/reciclo/activities/MainActivity.java
+++ app/src/main/java/com/wilderpereira/reciclo/activities/MainActivity.java
@@ -2,9 +2,16 @@ package com.wilderpereira.reciclo.activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.wilderpereira.reciclo.adapters.ViewPagerAdapter;
 import com.wilderpereira.reciclo.fragments.ListFragment;
@@ -17,17 +24,25 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
+    //Create array Resource
+    private String[] drawerOptions = {"Test1", "Test2"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar).findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
 
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
+
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
@@ -47,6 +62,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        setupDrawer();
+
+
     }
 
     private void setupViewPager(final ViewPager viewPager){
@@ -55,5 +77,55 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(ListFragment.newInstance(ListFragment.LIST_MODE_DEFAULT, viewPager), getString(R.string.recycle));
         adapter.addFragment(ListFragment.newInstance(ListFragment.LIST_MODE_FAVORITES, viewPager), getString(R.string.favorites));
         viewPager.setAdapter(adapter);
+    }
+
+    private void setupDrawer(){
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, drawerOptions));
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar
+                , R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+        /** Swaps fragments in the main content view */
+        private void selectItem(int position) {
+
+        /*Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();*/
+
+            // Highlight the selected item, update the title, and close the drawer
+            mDrawerList.setItemChecked(position, true);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        }
+
     }
 }
