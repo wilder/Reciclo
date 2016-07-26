2b1fcf20a9185b54071180198bfd82c4cdbf2174
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-25 23:18:46 -0300

    Retrieving preparation and recipe resources from firebase

--- app/src/main/java/com/wilderpereira/reciclo/Activities/RecipeActivity.java
+++ app/src/main/java/com/wilderpereira/reciclo/Activities/RecipeActivity.java
@@ -2,33 +2,43 @@ package com.wilderpereira.reciclo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.wilderpereira.reciclo.models.Preparation;
 import com.wilderpereira.reciclo.models.Recipe;
 import com.wilderpereira.reciclo.models.Resource;
 import com.wilderpereira.reciclo.R;
+import com.wilderpereira.reciclo.models.Steps;
 
 import java.util.List;
 
-public class RecipeActivity extends AppCompatActivity {
+public class  RecipeActivity extends AppCompatActivity {
 
-    public ImageView itemImage;
-    public TextView itemName;
-    public TextView owner;
-    public TextView favoriteCount;
-    public TextView recyleCount;
-    public LinearLayout linearIngredients;
-    public LinearLayout linearPreparation;
-    public Button btnReciclo;
+    private ImageView itemImage;
+    private TextView itemName;
+    private TextView owner;
+    private TextView favoriteCount;
+    private TextView recyleCount;
+    private LinearLayout linearIngredients;
+    private LinearLayout linearPreparation;
+    private Button btnReciclo;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Bundle extras = getIntent().getExtras();
         Recipe recipe = (Recipe) extras.getSerializable(getString(R.string.recipe_extra_key));
@@ -47,22 +57,58 @@ public class RecipeActivity extends AppCompatActivity {
         favoriteCount.setText(recipe.getRecycleCount()+""); //TODO: Change to favorite count (Also add on firebase)
         recyleCount.setText(recipe.getRecycleCount()+"");
 
-        String preparation = recipe.getPreparation();
-        /*for(){
+        String preparationId = recipe.getPreparation();
+        getPreparation(preparationId);
 
-        }*/
+        String resources = recipe.getResource();
+        getResources(resources);
+    }
 
-        String resources = recipe.getResouces();
-        /*for (Resource resource : resources){
-            int i = 1;
-            TextView tvResource = new TextView(this);
-            tvResource.setText("x"+resource.getAmount()+ " "+resource.getName());
-            tvResource.setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
-            linearIngredients.addView(tvResource);
-            i++;
-        }*/
+    public void getResources(String resourcesId){
+        mDatabase.child("resources").child(resourcesId).child("resource").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot resourceSnapshot: dataSnapshot.getChildren()) {
+                            Resource res = resourceSnapshot.getValue(Resource.class);
+                            addTextViewToLinearLayout(linearIngredients,res.getName() + " x"+res.getAmount());
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("RecipeActivity", "getUser:onCancelled", databaseError.toException());
+                    }
+                });
+    }
+
+    public void getPreparation(String preparationId){
+        mDatabase.child("preparation").child(preparationId).child("steps").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot preparationSnapshot: dataSnapshot.getChildren()) {
+                            Steps step = preparationSnapshot.getValue(Steps.class);
+                            addTextViewToLinearLayout(linearPreparation,step.getDescription());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("RecipeActivity", "getUser:onCancelled", databaseError.toException());
+                    }
+                });
+    }
+
+    //TODO: Move to helper class
+    private void addTextViewToLinearLayout(LinearLayout container, String text){
+        TextView textView = new TextView(this);
+        textView.setText(" - "+text);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        container.addView(textView);
     }
 
 }
