770d3091a5fc5904de7a8d2f82afba42620c587c
Wilder Pereira <wilder_roberto@hotmail.com>
2016-10-05 21:39:28 -0300

    moved createEmptyStock() to stockUtils

--- app/src/main/java/com/wilderpereira/reciclo/utils/FirebaseUtils.java
+++ app/src/main/java/com/wilderpereira/reciclo/utils/FirebaseUtils.java
@@ -15,24 +15,4 @@ public class FirebaseUtils {
 
     public static final String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-    /** Returns stock key that is used to create the new users' stock */
-    public static void createEmptyStock(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = Utils.getDatabase().getReference();
-
-        //Default items TODO: Create default items on firebase
-        Map<String, Object> stockItensNode = new HashMap<>();
-        //TODO: add type to item
-        stockItensNode.put("glass_4x4", new StockItem("glass_4x4",0,"https://goo.gl/M3h080").toMap());
-        stockItensNode.put("2l_bottle", new StockItem("2l_bottle",0,"https://goo.gl/1mYqPq").toMap());
-        stockItensNode.put("4A_paper", new StockItem("4A_paper",0,"https://goo.gl/N39MCC").toMap());
-        stockItensNode.put("cardboard", new StockItem("cardboard",0,"https://goo.gl/7GBsSi").toMap());
-        stockItensNode.put("wood", new StockItem("wood",0,"https://goo.gl/4yF4El").toMap());
-
-        //Creates the new node
-        Map<String, Object> stockNode = new HashMap<>();
-        stockNode.put("stocks/"+UID+"/itens",stockItensNode);
-        myRef.updateChildren(stockNode);
-
-    }
 }
--- app/src/main/java/com/wilderpereira/reciclo/utils/StockUtils.java
+++ app/src/main/java/com/wilderpereira/reciclo/utils/StockUtils.java
@@ -3,6 +3,7 @@ package com.wilderpereira.reciclo.utils;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.wilderpereira.reciclo.adapters.StockAdapter;
 import com.wilderpereira.reciclo.models.StockItem;
 
@@ -24,5 +25,26 @@ public class StockUtils {
         databaseReference.updateChildren(childUpdates);
         stockItem.setAmount(newAmount);
         tvAmount.setText(""+newAmount);
+    }
+
+    /** Returns stock key that is used to create the new users' stock */
+    public static void createEmptyStock(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = Utils.getDatabase().getReference();
+
+        //Default items TODO: Create default items on firebase
+        Map<String, Object> stockItensNode = new HashMap<>();
+        //TODO: add type to item
+        stockItensNode.put("glass_4x4", new StockItem("glass_4x4",0,"https://goo.gl/M3h080").toMap());
+        stockItensNode.put("2l_bottle", new StockItem("2l_bottle",0,"https://goo.gl/1mYqPq").toMap());
+        stockItensNode.put("4A_paper", new StockItem("4A_paper",0,"https://goo.gl/N39MCC").toMap());
+        stockItensNode.put("cardboard", new StockItem("cardboard",0,"https://goo.gl/7GBsSi").toMap());
+        stockItensNode.put("wood", new StockItem("wood",0,"https://goo.gl/4yF4El").toMap());
+
+        //Creates the new node
+        Map<String, Object> stockNode = new HashMap<>();
+        stockNode.put("stocks/"+uid+"/itens",stockItensNode);
+        myRef.updateChildren(stockNode);
+
     }
 }
