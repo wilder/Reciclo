26f65f877f5c0fd5b5db1f61cb7cdb66169edb89
Wilder Pereira <wilder_roberto@hotmail.com>
2016-10-05 21:38:16 -0300

    updated createEmptyStock() to match firebase's new node

--- app/src/main/java/com/wilderpereira/reciclo/utils/FirebaseUtils.java
+++ app/src/main/java/com/wilderpereira/reciclo/utils/FirebaseUtils.java
@@ -23,11 +23,11 @@ public class FirebaseUtils {
         //Default items TODO: Create default items on firebase
         Map<String, Object> stockItensNode = new HashMap<>();
         //TODO: add type to item
-        stockItensNode.put("glass", new StockItem("glass_4x4",0,"https://goo.gl/M3h080").toMap());
-        stockItensNode.put("plastic", new StockItem("2l_bottle",0,"https://goo.gl/1mYqPq").toMap());
-        stockItensNode.put("paper", new StockItem("4A_paper",0,"https://goo.gl/N39MCC").toMap());
-        stockItensNode.put("paper", new StockItem("cardboard",0,"https://goo.gl/7GBsSi").toMap());
-        stockItensNode.put("others", new StockItem("wood",0,"https://goo.gl/4yF4El").toMap());
+        stockItensNode.put("glass_4x4", new StockItem("glass_4x4",0,"https://goo.gl/M3h080").toMap());
+        stockItensNode.put("2l_bottle", new StockItem("2l_bottle",0,"https://goo.gl/1mYqPq").toMap());
+        stockItensNode.put("4A_paper", new StockItem("4A_paper",0,"https://goo.gl/N39MCC").toMap());
+        stockItensNode.put("cardboard", new StockItem("cardboard",0,"https://goo.gl/7GBsSi").toMap());
+        stockItensNode.put("wood", new StockItem("wood",0,"https://goo.gl/4yF4El").toMap());
 
         //Creates the new node
         Map<String, Object> stockNode = new HashMap<>();
