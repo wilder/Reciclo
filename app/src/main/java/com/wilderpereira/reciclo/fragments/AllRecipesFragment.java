ea14ff62f5e30d16189f6c59500be00a737259d6
Wilder Pereira <wilder_roberto@hotmail.com>
2016-10-05 22:29:16 -0300

    Created fragment for all recipes

---
+++ app/src/main/java/com/wilderpereira/reciclo/fragments/AllRecipesFragment.java
@@ -0,0 +1,14 @@
+package com.wilderpereira.reciclo.fragments;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllRecipesFragment extends MainListFragment {
+
+    public AllRecipesFragment() {
+    }
+
+}
