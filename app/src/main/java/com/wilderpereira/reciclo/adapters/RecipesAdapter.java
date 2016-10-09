b06d2af9507c7f82ea5ac34106d92e322afa79e4
Wilder Pereira <wilder_roberto@hotmail.com>
2016-10-09 00:21:15 -0300

    added query/database reference to adapter

--- app/src/main/java/com/wilderpereira/reciclo/adapters/RecipesAdapter.java
+++ app/src/main/java/com/wilderpereira/reciclo/adapters/RecipesAdapter.java
@@ -3,6 +3,7 @@ package com.wilderpereira.reciclo.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.wilderpereira.reciclo.R;
 import com.wilderpereira.reciclo.activities.RecipeActivity;
 import com.wilderpereira.reciclo.models.Recipe;
@@ -30,6 +32,7 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
     private List<Recipe> mDataset;
     private Recipe recipe;
     private Context context;
+    private Query query;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, andØØ
@@ -47,6 +50,7 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
         public ImageButton btnFShare;
         public String star;
 
+
         public ViewHolder(View v) {
             super(v);
             itemName = (TextView) v.findViewById(R.id.tv_item_name);
@@ -55,14 +59,15 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
             btnFavorite = (ImageButton) v.findViewById(R.id.ib_favorite);
             btnFShare = (ImageButton) v.findViewById(R.id.ib_share);
             linearItem = (LinearLayout) v.findViewById(R.id.linear_item);
-            imgStar = (ImageView) v.findViewById(R.id.img_favorite);
+            imgStar = (ImageView) v.findViewById(R.id.iv_favorite);
             favoriteCount = (TextView) v.findViewById(R.id.tv_favorite_count);
             star = v.getResources().getString(R.string.stars);
         }
     }
 
-    public RecipesAdapter(List<Recipe> myDataset) {
+    public RecipesAdapter(List<Recipe> myDataset, Query query) {
         mDataset = myDataset;
+        this.query = query;
     }
 
     @Override
@@ -84,21 +89,16 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
 
 
         recipe = mDataset.get(position);
-        final DatabaseReference recipeRef = FirebaseUtils.getDatabase().getReference("recipes").child(recipe.getUid());
+        final DatabaseReference recipeRef = query.getRef().child(recipe.getUid());
 
         holder.itemName.setText(recipe.getName());
         holder.recycleCount.setText("Reciclado "+recipe.getRecycleCount()+" vezes.");
+        holder.favoriteCount.setText("Favoritado "+recipe.getFavoriteCount()+" vezes.");
 
+        Log.d("adapter", "recipe.favoritedBy.containsKey(FirebaseUtils.UID) "+recipe.favoritedBy.containsKey(FirebaseUtils.UID));
         if(recipe.favoritedBy.containsKey(FirebaseUtils.UID)){
             holder.imgStar.setImageResource(R.drawable.circle);
         }
-
-        holder.imgStar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Utils.onStarClicked(recipeRef, FirebaseUtils.UID);
-            }
-        });
 
         // Set click listener for the whole post view
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -108,6 +108,14 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
                 Intent intent = new Intent(v.getContext(),RecipeActivity.class);
                 intent.putExtra(context.getString(R.string.recipe_extra_key), recipe);
                 context.startActivity(intent);
+            }
+        });
+
+        holder.imgStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.onStarClicked(recipeRef, FirebaseUtils.UID);
+                //Log.d("click", "imgstar: ");
             }
         });
 
