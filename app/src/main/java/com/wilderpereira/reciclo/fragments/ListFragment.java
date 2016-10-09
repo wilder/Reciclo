0c77bf8d44d3d99e9697c3ff6c9d3012bf788740
Wilder Pereira <wilder_roberto@hotmail.com>
2016-10-09 00:22:20 -0300

    Load only recipes that user can make - MainListFragment

--- app/src/main/java/com/wilderpereira/reciclo/fragments/ListFragment.java
+++ app/src/main/java/com/wilderpereira/reciclo/fragments/ListFragment.java
@@ -5,11 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +19,6 @@ import com.wilderpereira.reciclo.adapters.RecipesAdapter;
 import com.wilderpereira.reciclo.models.Recipe;
 import com.wilderpereira.reciclo.models.StockItem;
 import com.wilderpereira.reciclo.utils.FirebaseUtils;
-import com.wilderpereira.reciclo.viewholder.ItemViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +32,9 @@ public abstract class ListFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private DatabaseReference mDatabase;
     private RecipesAdapter adapter;
-    private List<StockItem> stock = new ArrayList<>();
-    private List<Recipe> recipes;
+    List<StockItem> stock = new ArrayList<>();
+    List<Recipe> recipes;
+    String TAG = "ListFragment";
 
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -49,40 +48,41 @@ public abstract class ListFragment extends Fragment {
         loadItens();
 
         Query recipesQuery = getQuery(mDatabase);
-        adapter = new RecipesAdapter(recipes);
-
-        loadRecipes(recipesQuery);
+        adapter = new RecipesAdapter(recipes, recipesQuery);
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler);
         mRecyclerView.setAdapter(adapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        return view;
-    }
-
-    public abstract Query getQuery(DatabaseReference databaseReference);
-
-    private void loadRecipes(Query recipesQuery){
-
         recipesQuery.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 recipes.clear();
+                Log.d(TAG, "onDataChange");
                 for (DataSnapshot postSnapshot: snapshot.getChildren()) {
                     Recipe item = postSnapshot.getValue(Recipe.class);
-                    item.setUid(snapshot.getKey());
-                    recipes.add(item);
+                    item.setUid(postSnapshot.getKey());
+                    if(!shouldCheckStock() || item.canBeMade(stock)) {
+                        recipes.add(item);
+                    }
                     adapter.notifyDataSetChanged();
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.d(TAG, "onCancelled");
             }
 
         });
+
+        return view;
     }
+
+    public abstract Query getQuery(DatabaseReference databaseReference);
+
+    public abstract boolean shouldCheckStock();
+
 
     public void loadItens(){
         DatabaseReference databaseReference = FirebaseUtils.getDatabase().getReference().child("stocks/"+FirebaseUtils.UID+"/itens");
